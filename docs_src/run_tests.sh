d02895383182c0b13972d4253ed86975e1401641
Stas Bekman <stas@stason.org>
2018-10-15 16:07:59 -0700

    automatically install spacy's en default model if it's not there

--- docs_src/run_tests.sh
+++ docs_src/run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python -m pytest -p nbval "$@"
 
+# make sure spacy's en default model is installed before tests are run
+python -c 'import spacy; spacy.load("en")' 2>1 >> /dev/null || python -m spacy download en
+
+python -m pytest -p nbval "$@"
