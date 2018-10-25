46ccfab5949e579546316abf92d7a0c5b9a250d1
Stas Bekman <stas@stason.org>
2018-10-24 22:10:36 -0700

    split the data from the tool - the tool should remain the same, data will change

--- tools/make_sidebar.py
+++ tools/make_sidebar.py
@@ -1,91 +1,19 @@
 #!/usr/bin/env python
 
-# ## Sidebar creation thingy
+# create docs/_data/sidebars/home_sidebar.yml
+# from   docs_src/sidebar/sidebar_data.py
+#
+# usage: tools/make_sidebar.py
 
-import yaml
+import yaml, os, sys
+from pathlib import Path
 
-# Just a little tool to create the jekyll sidebar from a manually created dict.
-# Here's the dict defining the structure:
+# make sure we are under the root of the project
+cur_dir = Path(".").resolve().name
+if (cur_dir == "tools"): os.chdir("..")
 
-sidebar_d = {
-    'Getting started': {
-        'Installation': 'https://github.com/fastai/fastai/blob/master/README.md#installation',
-        'Troubleshooting': 'https://docs-dev.fast.ai/troubleshoot',
-    },
-    'Training': {
-        'Overview': 'training',
-        'basic_train': 'basic_train',
-        'train': 'train',
-        'metrics': 'metrics',
-        'callback': 'callback',
-        '': {
-            'callbacks': {
-                'Overview': 'callbacks',
-                'HookCallback': 'callbacks.hooks',
-                'MixedPrecision': 'callbacks.fp16',
-                'OneCycleScheduler': 'callbacks.one_cycle',
-                'LRFinder': 'callbacks.lr_finder',
-                'MixUpCallback': 'callbacks.mixup',
-                'RNNTrainer': 'callbacks.rnn',
-                'GeneralScheduler': 'callbacks.general_sched',
-            }
-        },
-    },
-    'Applications': {
-        'Overview': 'applications',
-        '': {
-            'vision': {
-                'Overview': 'vision',
-                'vision.learner': 'vision.learner',
-                'vision.transform': 'vision.transform',
-                'vision.image': 'vision.image',
-                'vision.data': 'vision.data',
-                'vision.model overview': 'vision.models',
-                'vision.models.unet': 'vision.models.unet'
-            }
-        },
-        'empty1': {
-            'text': {
-                'Overview': 'text',
-                'text.learner': 'text.learner',
-                'text.transform': 'text.transform',
-                'text.data': 'text.data',
-                'text.models': 'text.models'
-            },
-        },
-        'empty2': {
-            'tabular': {
-                'Overview': 'tabular',
-                'tabular.transform': 'tabular.transform',
-                'tabular.data': 'tabular.data',
-                'tabular.models': 'tabular.models'
-            },
-        },
-        'collab': 'collab',
-    },
-    'Core': {
-        'Overview': 'overview',
-        'basic_data': 'basic_data',
-        'layers': 'layers',
-        'datasets': 'datasets',
-        'core': 'core',
-        'torch_core': 'torch_core',
-    },
-    'Doc authoring': {
-        'Overview': 'gen_doc',
-        'gen_doc.gen_notebooks': 'gen_doc.gen_notebooks',
-        'gen_doc.nbdoc': 'gen_doc.nbdoc',
-        'gen_doc.convert2html': 'gen_doc.convert2html',
-    },
-    'Library development': {
-        'Overview': 'https://docs-dev.fast.ai/develop',
-        'Testing': 'https://docs-dev.fast.ai/test',
-        'Style guide': 'https://docs-dev.fast.ai/style',
-        'Abbreviation': 'https://docs-dev.fast.ai/abbr',
-        'Typing': 'fastai_typing',
-        'Packaging fastai': 'https://docs-dev.fast.ai/release'
-    }
-}
+sys.path.append(str(Path("docs_src")/"sidebar"))
+from sidebar_data import sidebar_d
 
 def _leaf(k,v):
     url = 'external_url' if "/" in v else 'url'
