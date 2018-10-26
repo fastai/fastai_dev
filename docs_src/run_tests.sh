d4f4429bed7382a51f6940197badd9055be84dd2
Stas Bekman <stas@stason.org>
2018-10-25 17:23:51 -0700

    fix a redirect bug

--- docs_src/run_tests.sh
+++ docs_src/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # make sure spacy's en default model is installed before tests are run
-python -c 'import spacy; spacy.load("en")' 2>1 >> /dev/null || python -m spacy download en
+python -c 'import spacy; spacy.load("en")' 2>&1 > /dev/null || python -m spacy download en
 
 python -m pytest -p nbval "$@"
