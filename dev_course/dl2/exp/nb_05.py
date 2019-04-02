2648cdeb4434b29885535df87079700526b8ed8c
Jeremy Howard <info@fast.ai>
2019-04-02 23:00:22 +0000

    lr find

--- dev_course/dl2/exp/nb_05.py
+++ dev_course/dl2/exp/nb_05.py
@@ -11,28 +11,6 @@ def create_learner(model_func, loss_func, data):
 
 def get_model_func(lr=0.5): return partial(get_model, lr=lr)
 
-class Recorder(Callback):
-    def begin_fit(self): self.lrs,self.losses,self.stats = [],[],[]
-
-    def after_batch(self):
-        if not self.in_train: return
-        self.lrs.append(self.opt.param_groups[-1]['lr'])
-        self.losses.append(self.loss.detach().cpu())
-
-    def plot_lr  (self): plt.plot(self.lrs)
-    def plot_loss(self): plt.plot(self.losses)
-
-class ParamScheduler(Callback):
-    _order=1
-    def __init__(self, pname, sched_func): self.pname,self.sched_func = pname,sched_func
-
-    def set_param(self):
-        for pg in self.opt.param_groups:
-            pg[self.pname] = self.sched_func(self.n_epochs/self.epochs)
-
-    def begin_batch(self):
-        if self.in_train: self.set_param()
-
 def annealer(f):
     def _inner(start, end): return partial(f, start, end)
     return _inner
@@ -47,6 +25,7 @@ def sched_no(start, end, pos):  return start
 @annealer
 def sched_exp(start, end, pos): return start * (end/start) ** pos
 
+#This monkey-patch is there to be able to plot tensors
 torch.Tensor.ndim = property(lambda x: len(x.shape))
 
 def combine_scheds(pcts, scheds):
@@ -59,3 +38,37 @@ def combine_scheds(pcts, scheds):
         actual_pos = (pos-pcts[idx]) / (pcts[idx+1]-pcts[idx])
         return scheds[idx](actual_pos)
     return _inner
+
+class Recorder(Callback):
+    def begin_fit(self):
+        self.lrs = [[] for _ in self.opt.param_groups]
+        self.losses = []
+
+    def after_batch(self):
+        if not self.in_train: return
+        for pg,lr in zip(self.opt.param_groups,self.lrs): lr.append(pg['lr'])
+        self.losses.append(self.loss.detach().cpu())
+
+    def plot_lr  (self, pgid=-1): plt.plot(self.lrs[pgid])
+    def plot_loss(self, skip_last=0): plt.plot(self.losses[:len(self.losses)-skip_last])
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs): self.pname,self.sched_funcs = pname,sched_funcs
+
+    def begin_fit(self):
+        if not isinstance(self.sched_funcs, (list,tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+
+    def set_param(self):
+        assert len(self.opt.param_groups)==len(self.sched_funcs)
+        for pg,f in zip(self.opt.param_groups,self.sched_funcs):
+            pg[self.pname] = f(self.n_epochs/self.epochs)
+
+    def begin_batch(self):
+        if self.in_train: self.set_param()
+
+def pg_dicts(pgs): return [{'params':o} for o in pgs]
+
+def cos_1cycle_anneal(start, high, end):
+    return [sched_cos(start, high), sched_cos(high, end)]
