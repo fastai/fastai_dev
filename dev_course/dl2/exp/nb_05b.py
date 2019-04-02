3ea550a7edae47661ff12b418413abb2b7489bc7
Jeremy Howard <info@fast.ai>
2019-04-02 20:31:03 +0000

    cancel exceptions

--- dev_course/dl2/exp/nb_05b.py
+++ dev_course/dl2/exp/nb_05b.py
@@ -109,3 +109,48 @@ class Runner():
         res = False
         for cb in sorted(self.cbs, key=lambda x: x._order): res = cb(cb_name) and res
         return res
+
+class AvgStatsCallback(Callback):
+    def __init__(self, metrics):
+        self.train_stats,self.valid_stats = AvgStats(metrics,True),AvgStats(metrics,False)
+
+    def begin_epoch(self):
+        self.train_stats.reset()
+        self.valid_stats.reset()
+
+    def after_loss(self):
+        stats = self.train_stats if self.in_train else self.valid_stats
+        with torch.no_grad(): stats.accumulate(self.run)
+
+    def after_epoch(self):
+        print(self.train_stats)
+        print(self.valid_stats)
+
+class Recorder(Callback):
+    def begin_fit(self):
+        self.lrs = [[] for _ in self.opt.param_groups]
+        self.losses = []
+
+    def after_batch(self):
+        if not self.in_train: return
+        for pg,lr in zip(self.opt.param_groups,self.lrs): lr.append(pg['lr'])
+        self.losses.append(self.loss.detach().cpu())
+
+    def plot_lr  (self, pgid=-1): plt.plot(self.lrs[pgid])
+    def plot_loss(self): plt.plot(self.losses)
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs): self.pname,self.sched_funcs = pname,sched_funcs
+
+    def begin_fit(self):
+        if not isinstance(self.sched_funcs, (list,tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+
+    def set_param(self):
+        assert len(self.opt.param_groups)==len(self.sched_funcs)
+        for pg,f in zip(self.opt.param_groups,self.sched_funcs):
+            pg[self.pname] = f(self.n_epochs/self.epochs)
+
+    def begin_batch(self):
+        if self.in_train: self.set_param()
