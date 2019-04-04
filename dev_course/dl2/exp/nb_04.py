480baa70313fe37dfa813c613b0e504c6cb6c723
Jeremy Howard <info@fast.ai>
2019-04-04 00:51:46 +0000

    lesson 10

--- dev_course/dl2/exp/nb_04.py
+++ dev_course/dl2/exp/nb_04.py
@@ -66,6 +66,7 @@ from typing import *
 def listify(o):
     if o is None: return []
     if isinstance(o, list): return o
+    if isinstance(o, str): return [o]
     if isinstance(o, Iterable): return list(o)
     return [o]
 
