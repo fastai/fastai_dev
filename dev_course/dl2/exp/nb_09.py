1edbb4a47a88795e6a887338aa935e470121399c
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-04-10 19:22:59 +0000

    Add prose and math

--- dev_course/dl2/exp/nb_09.py
+++ dev_course/dl2/exp/nb_09.py
@@ -6,70 +6,83 @@
 
 from exp.nb_08 import *
 
-class Optimizer(optim.Optimizer):
+class Recorder(Callback):
+    def begin_fit(self): self.lrs,self.losses = [],[]
+
+    def after_batch(self):
+        if not self.in_train: return
+        self.lrs.append(self.opt.hypers[-1]['lr'])
+        self.losses.append(self.loss.detach().cpu())
+
+    def plot_lr  (self): plt.plot(self.lrs)
+    def plot_loss(self): plt.plot(self.losses)
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_func):
+        self.pname,self.sched_func = pname,sched_func
+
+    def set_param(self):
+        for h in self.opt.hypers:
+            h[self.pname] = self.sched_func(self.n_epochs/self.epochs)
+
+    def begin_batch(self):
+        if self.in_train: self.set_param()
+
+def maybe_update(os, dest, f):
+    for o in os:
+        for k,v in f(o).items():
+            if k not in dest: dest[k] = v
+
+def get_defaults(d): return getattr(d,'_defaults',{})
+
+class Optimizer():
     def __init__(self, params, steppers, **defaults):
         self.steppers = listify(steppers)
-        stepper_defaults = {}
-        for stepper in self.steppers: stepper_defaults.update(getattr(stepper,'_defaults',{}))
-        super().__init__(params, {**stepper_defaults, **defaults})
+        maybe_update(self.steppers, defaults, get_defaults)
+        # might be a generator
+        self.param_groups = list(params)
+        # ensure params is a list of lists
+        if not isinstance(self.param_groups[0], list): self.param_groups = [self.param_groups]
+        self.hypers = [{**defaults} for p in self.param_groups]
+
+    def grad_params(self):
+        return [(p,hyper) for pg,hyper in zip(self.param_groups,self.hypers)
+            for p in pg if p.grad is not None]
+
+    def zero_grad(self):
+        for p,hyper in self.grad_params():
+            p.grad.detach_()
+            p.grad.zero_()
 
     def step(self):
-        for pg in self.param_groups:
-            for p in pg['params']:
-                if p.grad is not None: compose(p, self.steppers, pg=pg)
+        for p,hyper in self.grad_params(): compose(p, self.steppers, **hyper)
 
-class StatefulOptimizer(optim.Optimizer):
+class StatefulOptimizer(Optimizer):
     def __init__(self, params, steppers, stats=None, **defaults):
-        self.steppers,self.stats = listify(steppers),listify(stats)
-        base_defaults = {}
-        for stepper in self.steppers: base_defaults.update(getattr(stepper,'_defaults',{}))
-        for stat in self.stats:       base_defaults.update(getattr(stat,'_defaults',{}))
-        super().__init__(params, {**base_defaults, **defaults})
+        self.stats = listify(stats)
+        maybe_update(self.stats, defaults, get_defaults)
+        super().__init__(params, steppers, **defaults)
+        self.state = {}
 
     def step(self):
-        for pg in self.param_groups:
-            for p in pg['params']:
-                if p.grad is not None:
-                    if p not in self.state:
-                        init_state = {}
-                        for stat in self.stats: init_state.update(stat.init_state(p))
-                        self.state[p] = init_state
-                    state = self.state[p]
-                    for stat in self.stats: state = stat.update(p, pg, state)
-                    compose(p, self.steppers, pg=pg, state=state)
-                    self.state[p] = state
+        for p,hyper in self.grad_params():
+            if p not in self.state:
+                #Create a state for p and call all the statistics to initialize it.
+                self.state[p] = {}
+                maybe_update(self.stats, self.state[p], lambda o: o.init_state(p))
+            state = self.state[p]
+            for stat in self.stats: state = stat.update(p, state, **hyper)
+            compose(p, self.steppers, **state, **hyper)
+            self.state[p] = state
 
 class Stat():
     _defaults = {}
-    def init_state(self, p):        raise NotImplementedError
-    def update(self, p, pg, state): raise NotImplementedError
+    def init_state(self, p): raise NotImplementedError
+    def update(self, p, state, **kwargs): raise NotImplementedError
 
-class AverageGrad(Stat):
-    _defaults = dict(mom=0.9)
-
-    def init_state(self, p): return {'grad_avg': torch.zeros_like(p.grad.data)}
-    def update(self, p, pg, state):
-        state['grad_avg'].mul_(pg['mom']).add_(p.grad.data)
-        return state
-
-class WeightDecay():
-    _defaults = dict(wd=0.)
-    def __call__(self,p,pg,state):
-        p.data.mul_(1 - pg['lr'] * pg['wd'])
-        return p
-
-class L2_Reg():
-    _defaults = dict(wd=0.)
-    def __call__(self,p,pg,state):
-        p.grad.data.add_(pg['wd'], p.data)
-        return p
-
-def sgd_step(p, pg,state):
-    p.data.add_(-pg['lr'], p.grad.data)
-    return p
-
-def momentum_step(p, pg, state):
-    p.data.add_(-pg['lr'], state['grad_avg'])
+def momentum_step(p, lr, grad_avg, **kwargs):
+    p.data.add_(-lr, grad_avg)
     return p
 
 class AverageGrad(Stat):
@@ -77,9 +90,9 @@ class AverageGrad(Stat):
 
     def __init__(self, dampening:bool=False): self.dampening=dampening
     def init_state(self, p): return {'grad_avg': torch.zeros_like(p.grad.data)}
-    def update(self, p, pg, state):
-        pg['mom_damp'] = 1 - pg['mom'] if self.dampening else 1.
-        state['grad_avg'].mul_(pg['mom']).add_(pg['mom_damp'], p.grad.data)
+    def update(self, p, state, mom, **kwargs):
+        state['mom_damp'] = 1-mom if self.dampening else 1.
+        state['grad_avg'].mul_(mom).add_(state['mom_damp'], p.grad.data)
         return state
 
 class AverageSqrGrad(Stat):
@@ -87,23 +100,22 @@ class AverageSqrGrad(Stat):
 
     def __init__(self, dampening:bool=True): self.dampening=dampening
     def init_state(self, p): return {'sqr_avg': torch.zeros_like(p.grad.data)}
-    def update(self, p, pg, state):
-        pg['sqr_damp'] = 1 - pg['sqr_mom'] if self.dampening else 1.
-        state['sqr_avg'].mul_(pg['sqr_mom']).addcmul_(pg['sqr_damp'],p.grad.data,p.grad.data)
+    def update(self, p, state, sqr_mom, **kwargs):
+        state['sqr_damp'] = 1 - sqr_mom if self.dampening else 1.
+        state['sqr_avg'].mul_(sqr_mom).addcmul_(state['sqr_damp'], p.grad.data, p.grad.data)
         return state
 
 class StepCount(Stat):
     def init_state(self, p): return {'step': 0}
-    def update(self, p, pg, state):
+    def update(self, p, state, **kwargs):
         state['step'] += 1
         return state
 
 def debias(mom, damp, step): return damp * (1 - mom**step) / (1-mom)
 
-class AdamStep():
-    _defaults = dict(eps=1e-5)
-    def __call__(self, p, pg, state):
-        debias1 = debias(pg['mom'],     pg['mom_damp'], state['step'])
-        debias2 = debias(pg['sqr_mom'], pg['sqr_damp'], state['step'])
-        p.data.addcdiv_(-pg['lr'] / debias1, state['grad_avg'], (state['sqr_avg']/debias2).sqrt() + pg['eps'])
-        return p
+def adam_step(p, lr, mom, mom_damp, step, sqr_mom, sqr_damp, grad_avg, sqr_avg, eps, **kwargs):
+    debias1 = debias(mom,     mom_damp, step)
+    debias2 = debias(sqr_mom, sqr_damp, step)
+    p.data.addcdiv_(-lr / debias1, grad_avg, (sqr_avg/debias2).sqrt() + eps)
+    return p
+adam_step._defaults = dict(eps=1e-5)
