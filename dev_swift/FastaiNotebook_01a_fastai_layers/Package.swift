036a681c1f81f06c69856687699286c1e0708df9
Marc Rasi <marcrasi@google.com>
2019-04-12 10:29:12 -0700

    01_fastai_layers -> 01a_fastai_layers

--- dev_swift/FastaiNotebook_01a_fastai_layers/Package.swift
+++ dev_swift/FastaiNotebook_01a_fastai_layers/Package.swift
@@ -2,18 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "FastaiNotebook_01_fastai_layers",
+    name: "FastaiNotebook_01a_fastai_layers",
     products: [
-        .library(name: "FastaiNotebook_01_fastai_layers", targets: ["FastaiNotebook_01_fastai_layers"]),
+        .library(name: "FastaiNotebook_01a_fastai_layers", targets: ["FastaiNotebook_01a_fastai_layers"]),
     ],
     dependencies: [
         .package(path: "../FastaiNotebook_00_load_data"),
+        .package(path: "../FastaiNotebook_01_matmul"),
         .package(url: "https://github.com/mxcl/Path.swift", from: "0.16.1"),
         .package(url: "https://github.com/JustHTTP/Just", from: "0.7.1")
     ],
     targets: [
         .target(
-            name: "FastaiNotebook_01_fastai_layers",
+            name: "FastaiNotebook_01a_fastai_layers",
             dependencies: ["Just", "Path"]),
     ]
 )
--- dev_swift/FastaiNotebook_02a_why_sqrt5/Package.swift
+++ dev_swift/FastaiNotebook_02a_why_sqrt5/Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../FastaiNotebook_00_load_data"),
-        .package(path: "../FastaiNotebook_01_fastai_layers"),
         .package(path: "../FastaiNotebook_01_matmul"),
+        .package(path: "../FastaiNotebook_01a_fastai_layers"),
         .package(path: "../FastaiNotebook_02_fully_connected"),
         .package(url: "https://github.com/mxcl/Path.swift", from: "0.16.1"),
         .package(url: "https://github.com/JustHTTP/Just", from: "0.7.1")
