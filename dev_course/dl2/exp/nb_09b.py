5084ae8104054835b8cd006b650ac9c9fb6140cc
Jeremy Howard <j@howard.fm>
2019-04-14 08:27:36 -0700

    transfer learning

--- dev_course/dl2/exp/nb_09b.py
+++ dev_course/dl2/exp/nb_09b.py
@@ -7,35 +7,39 @@
 from exp.nb_09 import *
 
 class Learner():
-    def __init__(self, model, data, loss_func, opt=None, opt_func=sgd_opt, lr=None,
+    def __init__(self, model, data, loss_func, opt_func=sgd_opt, lr=1e-2,
                  cbs=None, cb_funcs=None):
-        assert opt or lr
-        if not opt: opt = opt_func(model.parameters(), lr=lr)
-        self.opt = opt
-        self.model,self.data,self.loss_func,self.opt = model,data,loss_func,opt
-        self.in_train,self.logger = False,print
+        self.model,self.data,self.loss_func,self.opt_func,self.lr = model,data,loss_func,opt_func,lr
+        self.in_train,self.logger,self.opt = False,print,None
 
-        cbs = listify(cbs)
-        for cbf in listify(cb_funcs):
-            cb = cbf()
-            setattr(self, cb.name, cb)
-            cbs.append(cb)
-        self.stop,self.cbs = False,[TrainEvalCallback()]+cbs
+        # NB: Things marked "NEW" are covered in lesson 12
+        # NEW: avoid need for set_runner
+        self.cbs = []
+        self.add_cb(TrainEvalCallback())
+        self.add_cbs(cbs)
+        self.add_cbs(cbf() for cbf in listify(cb_funcs))
+
+    def add_cbs(self, cbs):
+        for cb in listify(cbs): self.add_cb(cb)
+
+    def add_cb(self, cb):
+        cb.set_runner(self)
+        setattr(self, cb.name, cb)
+        self.cbs.append(cb)
+
+    def remove_cbs(self, cbs):
+        for cb in listify(cbs): self.cbs.remove(cb)
 
     def one_batch(self, i, xb, yb):
         try:
             self.iter = i
-            self.xb,self.yb = xb,yb
-            self('begin_batch')
-            self.pred = self.model(self.xb)
-            self('after_pred')
-            self.loss = self.loss_func(self.pred, self.yb)
-            self('after_loss')
+            # NEW: format to see similarities and differences
+            self.xb,self.yb = xb,yb;                        self('begin_batch')
+            self.pred = self.model(self.xb);                self('after_pred')
+            self.loss = self.loss_func(self.pred, self.yb); self('after_loss')
             if not self.in_train: return
-            self.loss.backward()
-            self('after_backward')
-            self.opt.step()
-            self('after_step')
+            self.loss.backward();                           self('after_backward')
+            self.opt.step();                                self('after_step')
             self.opt.zero_grad()
         except CancelBatchException: self('after_cancel_batch')
         finally: self('after_batch')
@@ -48,14 +52,18 @@ class Learner():
 
     def do_begin_fit(self, epochs):
         self.epochs,self.loss = epochs,tensor(0.)
-        for cb in self.cbs: cb.set_runner(self)
         self('begin_fit')
 
     def do_begin_epoch(self, epoch):
         self.epoch,self.dl = epoch,self.data.train_dl
         return self('begin_epoch')
 
-    def fit(self, epochs):
+    def fit(self, epochs, cbs=None, reset_opt=False):
+        # NEW: pass callbacks to fit() and have them removed when done
+        self.add_cbs(cbs)
+        # NEW: create optimizer on fit(), optionally replacing existing
+        if reset_opt or not self.opt: self.opt = self.opt_func(self.model.parameters(), lr=self.lr)
+
         try:
             self.do_begin_fit(epochs)
             for epoch in range(epochs):
@@ -68,7 +76,9 @@ class Learner():
                 self('after_epoch')
 
         except CancelTrainException: self('after_cancel_train')
-        finally: self('after_fit')
+        finally:
+            self('after_fit')
+            self.remove_cbs(cbs)
 
     ALL_CBS = {'begin_batch', 'after_pred', 'after_loss', 'after_backward', 'after_step',
         'after_cancel_batch', 'after_batch', 'after_cancel_epoch', 'begin_fit',
