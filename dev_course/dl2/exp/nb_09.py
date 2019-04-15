cee5cb7a219e6a6e0a22f7655a8a87d53afee51d
Jeremy Howard <j@howard.fm>
2019-04-15 05:35:13 -0700

    better discrim lr

--- dev_course/dl2/exp/nb_09.py
+++ dev_course/dl2/exp/nb_09.py
@@ -30,18 +30,14 @@ class Recorder(Callback):
 class ParamScheduler(Callback):
     _order=1
     def __init__(self, pname, sched_funcs):
-        self.pname,self.sched_funcs = pname,sched_funcs
-
-    def begin_fit(self):
-        if not isinstance(self.sched_funcs, (list,tuple)):
-            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
-
-    def set_param(self):
-        for f,h in zip(self.sched_funcs,self.opt.hypers):
-            h[self.pname] = f(self.n_epochs/self.epochs)
+        self.pname,self.sched_funcs = pname,listify(sched_funcs)
 
     def begin_batch(self):
-        if self.in_train: self.set_param()
+        if not self.in_train: return
+        fs = self.sched_funcs
+        if len(fs)==1: fs = fs*len(self.opt.param_groups)
+        pos = self.n_epochs/self.epochs
+        for f,h in zip(fs,self.opt.hypers): h[self.pname] = f(pos)
 
 class LR_Find(Callback):
     _order=1
