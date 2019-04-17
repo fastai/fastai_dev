affa8060b30af9d5ede6cef0f10459d7fa1448a6
Marc Rasi <marcrasi@google.com>
2019-04-16 20:59:54 -0700

    add notebook conforming array to differentiable

--- dev_swift/FastaiNotebook_01c_array_differentiable/Package.swift
+++ dev_swift/FastaiNotebook_01c_array_differentiable/Package.swift
@@ -2,24 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "FastaiNotebook_04_callbacks",
+    name: "FastaiNotebook_01c_array_differentiable",
     products: [
-        .library(name: "FastaiNotebook_04_callbacks", targets: ["FastaiNotebook_04_callbacks"]),
+        .library(name: "FastaiNotebook_01c_array_differentiable", targets: ["FastaiNotebook_01c_array_differentiable"]),
     ],
     dependencies: [
         .package(path: "../FastaiNotebook_00_load_data"),
         .package(path: "../FastaiNotebook_01_matmul"),
         .package(path: "../FastaiNotebook_01a_fastai_layers"),
-        .package(path: "../FastaiNotebook_01c_array_differentiable"),
-        .package(path: "../FastaiNotebook_02_fully_connected"),
-        .package(path: "../FastaiNotebook_02a_why_sqrt5"),
-        .package(path: "../FastaiNotebook_03_minibatch_training"),
         .package(url: "https://github.com/mxcl/Path.swift", from: "0.16.1"),
         .package(url: "https://github.com/JustHTTP/Just", from: "0.7.1")
     ],
     targets: [
         .target(
-            name: "FastaiNotebook_04_callbacks",
+            name: "FastaiNotebook_01c_array_differentiable",
             dependencies: ["Just", "Path"]),
     ]
 )
