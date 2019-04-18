d7e2428def12310e579beb4e6c9cc8a17e30bcde
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-04-18 18:40:20 +0000

    Update to latest build

--- dev_swift/FastaiNotebook_02_fully_connected/Package.swift
+++ dev_swift/FastaiNotebook_02_fully_connected/Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .package(path: "../FastaiNotebook_00_load_data"),
         .package(path: "../FastaiNotebook_01_matmul"),
         .package(path: "../FastaiNotebook_01a_fastai_layers"),
-        .package(path: "../FastaiNotebook_01c_array_differentiable"),
         .package(url: "https://github.com/mxcl/Path.swift", from: "0.16.1"),
         .package(url: "https://github.com/JustHTTP/Just", from: "0.7.1")
     ],
