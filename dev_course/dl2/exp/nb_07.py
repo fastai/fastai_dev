3e04adc3a86a88d044162904d4dab0dacd77d83b
Jeremy Howard <j@fast.ai>
2019-04-23 22:26:05 +0000

    add outputs

--- dev_course/dl2/exp/nb_07.py
+++ dev_course/dl2/exp/nb_07.py
@@ -21,11 +21,44 @@ def get_learn_run(nfs, data, lr, layer, cbs=None, opt_func=None, uniform=False, 
     init_cnn(model, uniform=uniform)
     return get_runner(model, data, lr=lr, cbs=cbs, opt_func=opt_func)
 
-from torch.jit import ScriptModule, script_method, script
-from typing import *
-
 def conv_layer(ni, nf, ks=3, stride=2, bn=True, **kwargs):
     layers = [nn.Conv2d(ni, nf, ks, padding=ks//2, stride=stride, bias=not bn),
               GeneralRelu(**kwargs)]
     if bn: layers.append(nn.BatchNorm2d(nf, eps=1e-5, momentum=0.1))
     return nn.Sequential(*layers)
+
+class RunningBatchNorm(nn.Module):
+    def __init__(self, nf, mom=0.1, eps=1e-5):
+        super().__init__()
+        self.mom, self.eps = mom, eps
+        self.mults = nn.Parameter(torch.ones (nf,1,1))
+        self.adds  = nn.Parameter(torch.zeros(nf,1,1))
+        self.register_buffer('sums', torch.zeros(1,nf,1,1))
+        self.register_buffer('sqrs', torch.zeros(1,nf,1,1))
+        self.register_buffer('count', tensor(0.))
+        self.register_buffer('factor', tensor(0.))
+        self.register_buffer('offset', tensor(0.))
+        self.batch = 0
+
+    def update_stats(self, x):
+        bs,nc,*_ = x.shape
+        self.sums.detach_()
+        self.sqrs.detach_()
+        dims = (0,2,3)
+        s    = x    .sum(dims, keepdim=True)
+        ss   = (x*x).sum(dims, keepdim=True)
+        c    = s.new_tensor(x.numel()/nc)
+        mom1 = s.new_tensor(1 - (1-self.mom)/math.sqrt(bs-1))
+        self.sums .lerp_(s , mom1)
+        self.sqrs .lerp_(ss, mom1)
+        self.count.lerp_(c , mom1)
+        self.batch += bs
+        means = self.sums/self.count
+        varns = (self.sqrs/self.count).sub_(means*means)
+        if bool(self.batch < 20): varns.clamp_min_(0.01)
+        self.factor = self.mults / (varns+self.eps).sqrt()
+        self.offset = self.adds - means*self.factor
+
+    def forward(self, x):
+        if self.training: self.update_stats(x)
+        return x*self.factor + self.offset
