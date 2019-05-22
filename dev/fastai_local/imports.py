592ad03ed4c9ef588b6c47407f958ca993a890f5
Jeremy Howard <info@fast.ai>
2019-05-22 18:05:23 +0000

    refactor

--- dev/fastai_local/imports.py
+++ dev/fastai_local/imports.py
@@ -1,19 +1,38 @@
-import matplotlib.pyplot as plt,numpy as np,pandas as pd,scipy
-import torch,operator,sys,os,re,PIL,os,mimetypes,csv,itertools,json,shutil,glob,pickle,requests,yaml,tarfile,hashlib
+import operator,sys,os,re,os,mimetypes,csv,itertools,json,shutil,glob,pickle,tarfile,hashlib,itertools
+
 from typing import Iterable,Iterator,Generator,Callable,Sequence,List,Tuple,Union,Optional
-from torch import as_tensor,Tensor
-from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
-from numpy import array,ndarray
-from IPython.core.debugger import set_trace
 from pathlib import Path
 from collections import OrderedDict,defaultdict,Counter
 from enum import Enum,IntEnum
 from warnings import warn
 from functools import partial,reduce
-from typeguard import typechecked
-from fastai.gen_doc.nbdoc import show_doc
 from textwrap import TextWrapper
+
+# External modules
+import torch,matplotlib.pyplot as plt,numpy as np,pandas as pd,scipy
+import PIL,requests,yaml
+from typeguard import typechecked
 from fastprogress import progress_bar,master_bar
 
+from torch import as_tensor,Tensor
+from torch.utils.data import DataLoader,SequentialSampler,RandomSampler
+from numpy import array,ndarray
+from IPython.core.debugger import set_trace
+
+from fastai.gen_doc.nbdoc import show_doc
+
 NoneType = type(None)
+
+def all_equal(a,b):
+    "Compares whether `a` and `b` are the same length and have the same contents"
+    return all(equals(a_,b_) for a_,b_ in itertools.zip_longest(a,b))
+
+def equals(a,b):
+    "Compares `a` and `b` for equality; supports sublists, tensors and arrays too"
+    cmp = (torch.equal    if isinstance(a, Tensor  ) and a.dim() else 
+           np.array_equal if isinstance(a, ndarray ) else
+           operator.eq    if isinstance(a, str     ) else
+           all_equal     if isinstance(a, (list,tuple,Generator,Iterator)) else
+           operator.eq)
+    return cmp(a,b)
 
