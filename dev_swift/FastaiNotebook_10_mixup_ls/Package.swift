0fd9ba466f549b743b47e1726aa0cb020cb519b4
Jeremy Howard <j@fast.ai>
2019-06-24 13:50:46 -0400

    export xresnet

--- dev_swift/FastaiNotebook_10_mixup_ls/Package.swift
+++ dev_swift/FastaiNotebook_10_mixup_ls/Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "FastaiNotebook_10_mixup_ls",
-    products: [
-        .library(name: "FastaiNotebook_10_mixup_ls", targets: ["FastaiNotebook_10_mixup_ls"]),
-    ],
+name: "FastaiNotebook_10_mixup_ls",
+products: [
+.library(name: "FastaiNotebook_10_mixup_ls", targets: ["FastaiNotebook_10_mixup_ls"]),
+
+],
 dependencies: [
-    .package(path: "../FastaiNotebook_09_optimizer")
+.package(path: "../FastaiNotebook_09_optimizer")
 ],
 targets: [
-    .target(
-        name: "FastaiNotebook_10_mixup_ls",
-        dependencies: ["FastaiNotebook_09_optimizer"]),
-    ]
+.target(name: "FastaiNotebook_10_mixup_ls", dependencies: ["FastaiNotebook_09_optimizer"]),
+
+]
 )
