14164fb48ce4f188d1aa63ca04d6d6f43c6ed117
Brennan Saeta <brennan.saeta@gmail.com>
2019-06-26 23:38:01 +0000

    Update install_cv4.sh to gracefully fall back to apt-get if apt-fast is not there. Also inform the user that the installation was successful.

--- dev_swift/SwiftCV/install/install_cv4.sh
+++ dev_swift/SwiftCV/install/install_cv4.sh
@@ -2,8 +2,14 @@ set -ex
 
 OPENCV_VERSION='4.1.0'
 
-sudo apt-fast -y update
-sudo apt-fast install -y build-essential cmake zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libopenexr-dev libgdal-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev libtbb-dev libeigen3-dev python-dev python-tk python-numpy python3-dev python3-tk python3-numpy unzip wget libopenblas-dev libopenblas-base liblapacke-dev libatlas-base-dev liblapack-dev libblas-dev
+APT_PROGRAM="apt-fast"
+command -v $APT_PROGRAM > /dev/null 2>&1 || {
+  echo "Falling back to apt-get as apt-fast is not installed..."
+  APT_PROGRAM="apt-get"
+}
+
+sudo $APT_PROGRAM -y update
+sudo $APT_PROGRAM install -y build-essential cmake zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libopenexr-dev libgdal-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev libtbb-dev libeigen3-dev python-dev python-tk python-numpy python3-dev python3-tk python3-numpy unzip wget libopenblas-dev libopenblas-base liblapacke-dev libatlas-base-dev liblapack-dev libblas-dev
 sudo ln -s /usr/include/lapacke.h /usr/include/x86_64-linux-gnu # corrected path for the library
 
 mkdir -p ~/download
@@ -40,3 +46,4 @@ sudo make install
 sudo ldconfig
 popd
 
+echo "Success!"
