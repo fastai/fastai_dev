5116a3dd39fab653919fca3d7e783692cd2ca5bb
Eugene Burmako <burmako@google.com>
2019-06-25 18:03:33 -0700

    Export dev_swift/05b_early_stopping.ipynb

--- dev_swift/FastaiNotebook_05b_early_stopping/Package.swift
+++ dev_swift/FastaiNotebook_05b_early_stopping/Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "FastaiNotebook_05b_early_stopping",
-    products: [
-        .library(name: "FastaiNotebook_05b_early_stopping", targets: ["FastaiNotebook_05b_early_stopping"]),
-    ],
+name: "FastaiNotebook_05b_early_stopping",
+products: [
+.library(name: "FastaiNotebook_05b_early_stopping", targets: ["FastaiNotebook_05b_early_stopping"]),
+
+],
 dependencies: [
-    .package(path: "../FastaiNotebook_05_anneal")
+.package(path: "../FastaiNotebook_05_anneal")
 ],
 targets: [
-    .target(
-        name: "FastaiNotebook_05b_early_stopping",
-        dependencies: ["FastaiNotebook_05_anneal"]),
-    ]
+.target(name: "FastaiNotebook_05b_early_stopping", dependencies: ["FastaiNotebook_05_anneal"]),
+
+]
 )
