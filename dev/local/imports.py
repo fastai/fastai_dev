9e38a1196e8464d3e3e5bc87d27c1236bc448076
Jeremy Howard <j@fast.ai>
2019-07-25 11:17:36 -0700

    imports

--- dev/local/imports.py
+++ dev/local/imports.py
@@ -1,5 +1,5 @@
 import io,operator,sys,os,re,os,mimetypes,csv,itertools,json,shutil,glob,pickle,tarfile,collections
-import hashlib,itertools,types,random,inspect,functools,random,time,math,copy,bz2,types,typing,numbers
+import hashlib,itertools,types,random,inspect,functools,random,time,math,copy,bz2,types,typing,numbers,string
 
 from contextlib import redirect_stdout,contextmanager
 from typing import Iterable,Iterator,Generator,Callable,Sequence,List,Tuple,Union,Optional
