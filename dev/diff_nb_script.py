37d74e9b19f876de7cf09130b7eb33591519d191
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-09-06 08:41:39 -0700

    Add diff script

---
+++ dev/diff_nb_script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from local.notebook.export import diff_nb_script
+from os import environ
+from local.script import *
+
+@call_parse
+def main(
+    lib_folder:Param("The folder where the library is", str)="local"
+):
+    diff_nb_script(lib_folder=lib_folder)
