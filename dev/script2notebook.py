ffa01e0da394b6e093eae297a5bb6ab61266f2e5
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-09-06 07:31:38 -0700

    Script2notebook script

---
+++ dev/script2notebook.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from local.notebook.export import script2notebook
+from os import environ
+from local.script import *
+
+@call_parse
+def main(
+    folder:Param("The folder where the library is", str)="local"
+):
+    script2notebook(folder)
