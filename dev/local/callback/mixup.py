f60d6d518acb5f913b9de8486fdaf8da3908a088
Sylvain Gugger <sylvain.gugger@gmail.com>
2019-10-01 07:15:22 -0700

    First fix in mixup

--- dev/local/callback/mixup.py
+++ dev/local/callback/mixup.py
@@ -21,10 +21,10 @@ def reduce_loss(loss, reduction='mean'):
 from torch.distributions.beta import Beta
 
 class MixUp(Callback):
-    _order = 90 #Runs after normalization and cuda
-    def __init__(self, alpha = 0.4): self.distrib = Beta(tensor([alpha]), tensor([alpha]))
+    run_after=[Normalize, Cuda]
+    def __init__(self, alpha=0.4): self.distrib = Beta(tensor([alpha]), tensor([alpha]))
 
-    def begin_fit(self): self.old_loss_func,self.learn.loss_func = self.loss_func,self.loss_func
+    def begin_fit(self): self.old_loss_func,self.learn.loss_func = self.learn.loss_func,self.loss_func
 
     def begin_batch(self):
         if not self.training: return #Only mixup things during training
@@ -32,15 +32,15 @@ class MixUp(Callback):
         lam = torch.stack([lam, 1-lam], 1)
         self.lam = lam.max(1)[0][:,None,None,None]
         shuffle = torch.randperm(self.y.size(0)).to(self.x.device)
-        xb1,self.yb1 = self.x[shuffle],self.y[shuffle]
-        self.learn.xb = (torch.lerp(xb1, self.xb, self.lam),)
+        xb1,self.yb1 = tuple(x[shuffle] for x in self.xb),tuple(y[shuffle] for y in self.yb)
+        self.learn.xb = tuple(torch.lerp(x1, x, self.lam) for x,x1 in zip(self.xb, xb1))
 
     def after_fit(self): self.run.loss_func = self.old_loss_func
 
-    def loss_func(self, pred, yb):
-        if not self.in_train: return self.old_loss_func(pred, yb)
+    def loss_func(self, pred, *yb):
+        if not self.in_train: return self.old_loss_func(pred, *yb)
         with NoneReduce(self.old_loss_func) as loss_func:
-            loss1 = loss_func(pred, yb)
-            loss2 = loss_func(pred, self.yb1)
+            loss1 = loss_func(pred, *yb)
+            loss2 = loss_func(pred, *self.yb1)
         loss = torch.lerp(loss2, loss1, self.lam)
         return reduce_loss(loss, getattr(self.old_loss_func, 'reduction', 'mean'))
