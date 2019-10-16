c4e2e5f8937c9b8f4ff0f10db7893a65b15c95ec
Jeremy Howard <j@fast.ai>
2019-10-15 17:43:22 -0700

    img_pct_window

--- dev/local/medical/imaging.py
+++ dev/local/medical/imaging.py
@@ -123,7 +123,14 @@ def _split_elem(res,k,v):
 
 #Cell
 @patch
-def as_dict(self:DcmDataset, px_summ=True):
+def pct_in_window(dcm:DcmDataset, w, l):
+    "% of pixels in the window `(w,l)`"
+    px = dcm.scaled_px
+    return ((px > l-w//2) & (px < l+w//2)).float().mean().item()
+
+#Cell
+@patch
+def as_dict(self:DcmDataset, px_summ=True, window=dicom_windows.brain):
     pxdata = (0x7fe0,0x0010)
     vals = [self[o] for o in self.keys() if o != pxdata]
     its = [(v.keyword,v.value) for v in vals]
@@ -135,6 +142,7 @@ def as_dict(self:DcmDataset, px_summ=True):
     try:
         pxs = self.pixel_array
         for f in stats: res['img_'+f] = getattr(pxs,f)()
+        res['img_pct_window'] = self.pct_in_window(*window)
     except Exception as e:
         for f in stats: res['img_'+f] = 0
         print(res,e)
@@ -142,9 +150,10 @@ def as_dict(self:DcmDataset, px_summ=True):
     return res
 
 #Cell
-def _dcm2dict(px_summ, fn): return fn.dcmread().as_dict(px_summ)
+def _dcm2dict(fn, **kwargs): return fn.dcmread().as_dict(**kwargs)
 
 #Cell
 @delegates(parallel)
-def _from_dicoms(cls, fns, px_summ=True, **kwargs): return pd.DataFrame(parallel(partial(_dcm2dict,px_summ), fns, **kwargs))
+def _from_dicoms(cls, fns, n_workers=0, **kwargs):
+    return pd.DataFrame(parallel(_dcm2dict, fns, n_workers=n_workers, **kwargs))
 pd.DataFrame.from_dicoms = classmethod(_from_dicoms)
